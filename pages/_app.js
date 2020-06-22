63096bd9a7557634c00dfeda52a30477b737e960
zackdk <zackvvzackvv@gmail.com>
2020-06-22 17:51:22 +0800

    style

--- pages/_app.js
+++ pages/_app.js
@@ -6,8 +6,8 @@ import Head from 'next/head';
 import NProgress from 'nprogress';
 import Router from 'next/router';
 Router.events.on('routeChangeStart', (url) => {
-  console.log(`Loading: ${url}`)
-  NProgress.start()
+  console.log(`Loading: ${url}`);
+  NProgress.start();
 });
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
