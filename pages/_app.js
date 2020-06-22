5c237f694d4767636824f41e48cd8e72afde7119
zackdk <zackvvzackvv@gmail.com>
2020-06-22 17:48:19 +0800

    进度条

--- pages/_app.js
+++ pages/_app.js
@@ -3,13 +3,22 @@ import 'less/styles.less';
 import 'less/normalize.less';
 import Store from '@/store';
 import Head from 'next/head';
-// This default export is required in a new `pages/_app.js` file.
+import NProgress from 'nprogress';
+import Router from 'next/router';
+Router.events.on('routeChangeStart', (url) => {
+  console.log(`Loading: ${url}`)
+  NProgress.start()
+});
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
+
 export default function MyApp({ Component, pageProps }) {
   return (
     <Store.Provider>
       <Head>
         <title>佩奇搜索</title>
         <meta name="viewport" content="width=device-width" />
+        <link rel="stylesheet" type="text/css" href="/npprogress.css" />
       </Head>
       <Component {...pageProps} />
     </Store.Provider>
